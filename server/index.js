39fa2e4324a7ec3c057311e03f6769372647b7a3
zhangqippp <zhangqicode@gmail.com>
2018-10-27 15:53:29 +0800

    server start

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 
+const express = require('express');
+
+const app = express();
+
 class ZZServer {
+
+  async init() {
+    app.get('/', (req, res) => res.send('Hello World!'));
+    app.get('/more', (req, res) => res.send('get more ...'));
+  }
+
   async start() {
     console.log('server start ...');
+    await this.init();
+    app.listen(3000, () => console.log(`Example app listening on port 3000 !`));
   }
 }
 
